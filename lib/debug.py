cbf5efef2877bf0d635803dfd9b54259405f305c
heebatullah <heebahassan182@gmail.com>
2025-12-03 19:59:48 +0300

    Add debug.py for interactive testing of Company, Dev, and Freebie models

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
-from models import Company, Dev
+# from models import Company, Dev
+
+# if __name__ == '__main__':
+#     engine = create_engine('sqlite:///freebies.db')
+#     import ipdb; ipdb.set_trace()
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///freebies.db')
+    
+    # Create a session to query the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
     import ipdb; ipdb.set_trace()
