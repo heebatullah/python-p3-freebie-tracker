3b0fd727a614ee3de5f90eb7b92f01a920e8c4c9
heebatullah <heebahassan182@gmail.com>
2025-12-04 11:30:30 +0300

    Fixes issues on the create_company and create_freebies.py files and creates freebies.db

---
+++ lib/migrations/versions/202511301245_create_freebies.py
@@ -0,0 +1,20 @@
+from alembic import op
+import sqlalchemy as sa
+
+revision = '202511301245'
+down_revision = '5f72c58bf48c'
+branch_labels = None
+depends_on = None
+
+def upgrade()-> None:
+    op.create_table(
+        'freebies',
+        sa.Column('id', sa.Integer(), primary_key=True),
+        sa.Column('item_name', sa.String(), nullable=False),
+        sa.Column('value', sa.Integer(), nullable=False),
+        sa.Column('company_id', sa.Integer(), sa.ForeignKey('companies.id'), nullable=False),
+        sa.Column('dev_id', sa.Integer(), sa.ForeignKey('devs.id'), nullable=False)
+    )
+
+def downgrade():
+    op.drop_table('freebies')   
